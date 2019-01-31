c5faf254a91b76c7443e904cf35d57cf736f75cd
Alex Kozak <a.kozak1@outlook.com>
2019-01-31 14:21:32 -0500

    Implemented score traking and win conditions

--- DartScoreTracker/app/src/main/java/com/example/brushing0895/dartscoretracker/MainScreen.java
+++ DartScoreTracker/app/src/main/java/com/example/brushing0895/dartscoretracker/MainScreen.java
@@ -9,44 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
-
-    //private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    //mTextMessage.setText(R.string.title_board);
-                    return true;
-                case R.id.navigation_dashboard:
-                    //mTextMessage.setText(R.string.title_shots);
-                    return true;
-                case R.id.navigation_notifications:
-                    //mTextMessage.setText(R.string.title_win);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        //mTextMessage = (TextView)findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView)findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
         /*  Seek Bar for multiplier Settings   */
         /*  Multiplier: 1 , 2, 3          */
 
-        SeekBar multiplierBar = (SeekBar)findViewById(R.id.seekBarMultiplier);
+        final SeekBar multiplierBar = (SeekBar)findViewById(R.id.seekBarMultiplier);
         multiplierBar.incrementProgressBy(1);
         multiplierBar.setMax(2);
         multiplierBar.setProgress(0);
@@ -91,17 +66,62 @@ public class MainScreen extends AppCompatActivity {
             }
         });
 
+        final Button btn1 = ((Button)findViewById(R.id.btnShot1));
+        final Button btn2 = ((Button)findViewById(R.id.btnShot2));
+        final Button btn3 = ((Button)findViewById(R.id.btnShot3));
+        final Button btn50 = ((Button)findViewById(R.id.btn50));
+        final Button btn25 = ((Button)findViewById(R.id.btn25));
+        final Button btn0 = ((Button)findViewById(R.id.btn0));
+        final Button addShot = (Button)(findViewById(R.id.addShotButton));
+        final Button btnSubmitRound = (Button)(findViewById(R.id.btnSubmitRound));
 
-        Button addShot = (Button)(findViewById(R.id.addShotButton));
+        btnSubmitRound.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (btn1.getText().toString() != "" && btn2.getText().toString() != "" && btn3.getText().toString() != ""){
+                    int s1, s2, s3, roundScore, totalScore, oldRoundScore;
+                    s1 = Integer.parseInt(btn1.getText().toString());
+                    s2 = Integer.parseInt(btn2.getText().toString());
+                    s3 = Integer.parseInt(btn3.getText().toString());
+                    clearShots();
+
+                    roundScore = s1 + s2 + s3;
+                    Toast.makeText(getApplicationContext(),Integer.toString(roundScore),Toast.LENGTH_SHORT).show();
+                    totalScore = Integer.parseInt(((TextView)findViewById(R.id.lblRoundScorePreview)).getText().toString());
+                    ((TextView)findViewById(R.id.lblRoundScorePreview)).setText(Integer.toString(totalScore - roundScore));
+
+                }
+            }
+        });
+
+        btn50.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                addShot(50);
+                checkScore(true);
+            }
+        });
+
+        btn25.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                addShot(25);
+                checkScore(false);
+            }
+        });
+
+        btn0.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                addShot(0);
+            }
+        });
+
         addShot.setOnClickListener(new Button.OnClickListener(){
 
             @Override
             public void onClick(View v) {
                 //update button with total score
-
-                Button btn1 = ((Button)findViewById(R.id.buttonShot1));
-                Button btn2 = ((Button)findViewById(R.id.buttonShot2));
-                Button btn3 = ((Button)findViewById(R.id.buttonShot3));
 
                 //get multiplier value
                 int multiplierValue = ((SeekBar) findViewById(R.id.seekBarMultiplier)).getProgress()+1;
@@ -112,20 +132,113 @@ public class MainScreen extends AppCompatActivity {
                 //multiply together
                 int currentShotTotal = multiplierValue * shotPoint;
 
-                if (btn1.getText().toString() == ""){
-                    //button is empty so add total score to it
-                    btn1.setText(Integer.toString(currentShotTotal));
-                }
-                else if (btn2.getText().toString() == ""){
-                    //button is empty so add total score to it
-                    btn2.setText(Integer.toString(currentShotTotal));
-                }
-                else if (btn3.getText().toString() == ""){
-                    //button is empty so add total score to it
-                    btn3.setText(Integer.toString(currentShotTotal));
-                }
+                addShot(currentShotTotal);
+                checkScore(multiplierValue == 2);
             }
         });
+
+        btn1.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                btn1.setText("");
+            }
+        });
+        btn2.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                btn2.setText("");
+            }
+        });
+        btn3.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                btn3.setText("");
+            }
+        });
+    }
+
+    void checkScore(boolean isWinShot){
+        final Button btn1 = ((Button)findViewById(R.id.btnShot1));
+        final Button btn2 = ((Button)findViewById(R.id.btnShot2));
+        final Button btn3 = ((Button)findViewById(R.id.btnShot3));
+
+        int pr = pointsRemaining();
+        if (pr == 0 && isWinShot){
+            Toast.makeText(getApplicationContext(),"You Win!",Toast.LENGTH_SHORT).show();
+        }
+        else if (pr == 0 && !isWinShot)
+        {
+            Toast.makeText(getApplicationContext(),"BUST, You MUST double out or hit a bullseye!",Toast.LENGTH_SHORT).show();
+        }
+        else if (pr < 0 || pr == 1){
+            Button bSubmit = (Button)(findViewById(R.id.btnSubmitRound));
+            Toast.makeText(getApplicationContext(),"Bust!",Toast.LENGTH_SHORT).show();
+
+            btn3.setText("0");
+            if (pointsRemaining() > 1){
+                bSubmit.callOnClick();
+            }
+
+            btn2.setText("0");
+            if (pointsRemaining() > 1){
+                bSubmit.callOnClick();
+            }
+
+            clearShots();
+        }
+    }
+
+    void clearShots()
+    {
+        final Button btn1 = ((Button)findViewById(R.id.btnShot1));
+        final Button btn2 = ((Button)findViewById(R.id.btnShot2));
+        final Button btn3 = ((Button)findViewById(R.id.btnShot3));
+        btn1.setText("");
+        btn2.setText("");
+        btn3.setText("");
+    }
+
+    void addShot(int shotValue)
+    {
+        final Button btn1 = ((Button)findViewById(R.id.btnShot1));
+        final Button btn2 = ((Button)findViewById(R.id.btnShot2));
+        final Button btn3 = ((Button)findViewById(R.id.btnShot3));
+
+        if (btn1.getText().toString() == ""){
+            //button is empty so add total score to it
+            btn1.setText(Integer.toString(shotValue));
+        }
+        else if (btn2.getText().toString() == ""){
+            //button is empty so add total score to it
+            btn2.setText(Integer.toString(shotValue));
+        }
+        else if (btn3.getText().toString() == ""){
+            //button is empty so add total score to it
+            btn3.setText(Integer.toString(shotValue));
+        }
+    }
+
+    int pointsRemaining(){
+        int s1 = 0, s2 = 0, s3 = 0, totalScore = 0;
+        Button btn1 = ((Button)findViewById(R.id.btnShot1));
+        Button btn2 = ((Button)findViewById(R.id.btnShot2));
+        Button btn3 = ((Button)findViewById(R.id.btnShot3));
+
+        if (btn1.getText().toString() != ""){
+            //button is empty so add total score to it
+            s1 = Integer.parseInt(btn1.getText().toString());
+        }
+        if (btn2.getText().toString() != ""){
+            //button is empty so add total score to it
+            s2 = Integer.parseInt(btn2.getText().toString());
+        }
+        if (btn3.getText().toString() != ""){
+            //button is empty so add total score to it
+            s3 = Integer.parseInt(btn3.getText().toString());
+        }
+        totalScore = Integer.parseInt(((TextView)findViewById(R.id.lblRoundScorePreview)).getText().toString());
+
+        return totalScore - (s1 + s2 + s3);
     }
 
     void updateShotPreview(){
@@ -145,7 +258,4 @@ public class MainScreen extends AppCompatActivity {
         //update
         ((TextView)findViewById(R.id.textViewScorePreview)).setText(shotPreviewString);
     }
-
-
-
 }
